88f4b5bcfa9212c05c9f7637507a6837d7da571d
Miner Lozano <54141767+Apolosmllb@users.noreply.github.com>
2024-08-28 22:11:15 -0500

    chore: add facial recognition models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,52 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IamModule } from './iam/iam.module';
+import { RecognitionModule } from './recognition/recognition.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { CamerasModule } from './cameras/cameras.module';
+import { SharedModule } from './shared/shared.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'hs-db',
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: true,
+      migrationsRun: false,
+      logging: false,
+      //dropSchema: true ,
+      bigNumberStrings: false,
+      entities: [
+        process.env.ENVIRONMENT == 'prod'
+          ? './domain/entities/*.entity.js'
+          : './dist/domain/entities/*.entity.js',
+      ],
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    }),
+
+    IamModule,
+    RecognitionModule,
+    NotificationsModule,
+    CamerasModule,
+    SharedModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
