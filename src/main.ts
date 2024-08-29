44da4d350c99a7de87adee95128da2335d1073c9
Miner Lozano <54141767+Apolosmllb@users.noreply.github.com>
2024-08-28 22:14:18 -0500

    feat: add the main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as configv1 from 'dotenv';
+import { Logger } from '@nestjs/common';  
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  configv1.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Bootstrap');
+  app.setGlobalPrefix('api/v1');
+  const config = new DocumentBuilder()
+    .setTitle('RCA-API')
+    .setDescription('Rent Car Assists Api Documentation')
+    .setVersion('1.0')
+    .addTag('CRA')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors({ origin: '*' });
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  logger.log(`App running on port ${port}`);
 }
 bootstrap();
