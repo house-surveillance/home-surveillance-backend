6217ca1c650440037e4f41075f78896ab1983a8b
Miner Lozano <minermllb@gmail.com>
2024-09-05 15:26:02 -0500

    feat: add morgan config

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as configv1 from 'dotenv';
-import { Logger } from '@nestjs/common';  
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import morgan from 'morgan';
 
 async function bootstrap() {
   configv1.config();
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
+
+  app.use(morgan('combined'));
   app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   const config = new DocumentBuilder()
     .setTitle('RCA-API')
     .setDescription('Rent Car Assists Api Documentation')
